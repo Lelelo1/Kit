aa1b74d48f9a80530d92793f7614e44c0ba2a22d
Lelelo1 <leowetterek@Leos-Air-2.lan>
2019-08-02 21:26:02 +0200

    added missing files

---
+++ Kit/Extensions.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+namespace Namespace
+{
+    public static class Extensions 
+    {
+        /* Could use with 3 options: find nearest (any) Layout, find nearest Layout<View>, or find nearest ContentView */
+        /* works */
+        public static Element RootLayout(this Element element, bool layoutWithChildren = false)
+        {
+            return FindRootLayout(element, layoutWithChildren);
+        }
+        static Element FindRootLayout(Element element, bool layoutWithChildren)
+        {
+
+            if (element.Parent is Page)
+            {
+                // found end
+                if (layoutWithChildren)
+                {
+                    if (element is ContentView c)
+                    {
+                        return FindNearestLayoutWithChildren(c);
+                    }
+                    else
+                    {
+                        PrintError(element);
+                        return null;
+                    }
+                }
+                else
+                {
+
+                    return element;
+                }
+
+            }
+
+            return FindRootLayout(element.Parent, layoutWithChildren);
+        }
+        static Element FindNearestLayoutWithChildren(Element element)
+        {
+            if(element is ContentView c)
+            {
+
+                return FindNearestLayoutWithChildren(c.Content);
+            }
+            else if(element is Layout<View>)
+            {
+
+                return element;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /* Used by TopContentView And Draggable. Unlcear if there is other usecases for it */
+        /*
+        public static Layout PositionedOnLayout(this Element element)
+        {
+            var root = element.RootLayout();
+
+
+        }
+        */
+        /* list all layouts in the page */
+        public static List<Element> AllLayouts(this Element element)
+        {
+            var root = element.RootLayout();
+            Layouts = new List<Element>();
+            AddLayout(root);
+            var list = Layouts;
+            Layouts = null;
+            return list;
+        }
+        static List<Element> Layouts;
+        static Element AddLayout(Element element)
+        {
+
+            if(element is ContentView c)
+            {
+                Layouts.Add(c);
+                if(c.Content != null)
+                {
+                    AddLayout(c.Content);
+                }
+            }
+            else if(element is Layout<View> l)
+            {
+                Layouts.Add(l);
+                foreach(var child in l.Children)
+                {
+                    AddLayout(child);
+                }
+            }
+            else
+            {
+                // is control or TemplatedView or ContentPresenter
+            }
+            return element;
+        }
+        static void PrintError(Element element)
+        {
+            // was TemplatedView or ContentPresenter: https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/controls/layouts 
+            Console.WriteLine("Warning: Non supported layout type. Was: " + element + ". Should be of ContentView/Frame or Layout<View>");
+        }
+         
+    }
+}
---
+++ KitTest/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Namespace;
+using Xamarin.Forms;
+
+namespace KitTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+        void Handle_Clicked(object s, EventArgs e)
+        {
+            
+            var d = deepButton.RootLayout(false);
+            var a = anotherLabel.RootLayout(false);
+
+            // RootLayout of any element equals the root xaml layout
+            // and equals closest layout of type Layout<View> if specified with 'layoutWithChildren'
+            Console.WriteLine("d: " + d.Id + ", a: " + a.Id + " = " + root.Id);
+            
+            // Prints all layouts of the page
+            var list = deepButton.AllLayouts();
+            foreach(var layout in list)
+            {
+                Console.WriteLine(layout.AutomationId);
+            }
+        }
+    }
+}
